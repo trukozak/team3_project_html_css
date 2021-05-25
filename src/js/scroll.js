4de3dcc9e0024116a6aec5e338cee058cf29c094
trukozak <aleksandr.nepijvoda@gmail.com>
2021-05-25 20:51:30 +0300

    correct_scroll

--- src/js/scroll.js
+++
@@ -1,5 +0,0 @@
-$(".anchor").on("click", function (event) {
-            event.preventDefault();
-            var id  = $(this).attr('href');
-            $('body,html').animate({scrollTop: $(id).offset().top}, 700);
-        });
